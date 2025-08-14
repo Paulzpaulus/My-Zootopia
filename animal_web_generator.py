51589f11538e35c8dad16e48f184d7f85e8ef745
Paulspaulus <pauls.schweppe@gmail.com>
2025-08-14 08:34:35 +0200

    access files

--- animal_web_generator.py
+++ animal_web_generator.py
@@ -1,6 +1,32 @@
 import json 
 
-with open("animal_data.json", "r") as e:
-    animal_data = json.loads(e) 
+with open("animals_data.json", "r") as data:
+    animals_data = json.load(data) 
     
     
+    
+    
+        
+#print(type(animals_data))                 # list 
+#print(len(animals_data))                  # 19
+#first = animals_data[0]                   
+#print(type(first), first.keys())          # dict, dict_keys(['name', 'taxonomy', 'locations', 'characteristics'])
+#print(first["characteristics"].keys())    # dict_keys(['distinctive_feature', 'temperament', 'training', 'diet', 'average_litter_size', 
+#                                            'type', 'common_name', 'slogan', 'group', 'color', 'skin_type', 'lifespan'])
+#print(first["locations"][:3])             # list 
+
+def get_animal_info(animals_data):
+    for animal in animals_data:
+        if "name" in animal:
+            print(f"Name: {animal['name']}")
+        if "diet" in animal["characteristics"]:
+            print(f"Diet: {animal['characteristics']['diet']}")
+        if "locations" in animal:
+            print(f"Locations: {animal["locations"][0]}")
+        if "type" in animal["characteristics"]:
+            print(f"Type: {animal['characteristics']['type']}")
+    
+        print()
+
+get_animal_info(animals_data)
+
