27e7d586febf487e27ce9443e4d753b883590a2e
Paulspaulus <pauls.schweppe@gmail.com>
2025-08-20 10:03:14 +0200

    moved logic from top level

--- animal_web_generator.py
+++ animal_web_generator.py
@@ -1,39 +1,19 @@
-import json 
+from animal_web_generator_services import load_animals_data, load_animals_template, serialize_animal
 
-with open("animals_data.json", "r") as data:
-    animals_data = json.load(data) 
+if __name__ == "__main__":
+    animals_data = load_animals_data()
+    template = load_animals_template()
     
-
-with open("animals_template.html", "r") as template_file:
-    template = template_file.read() 
-
-
-def serialize_animal(animal):
-    name = animal.get('name')
-    diet = (animal.get('characteristics') or {}).get('diet', 'Unknown')
-    type_ = (animal.get('characteristics') or {}).get('type', 'Unknown')
-    locs = animal.get('locations') or []
-    location = locs[0] if locs else 'Unknown'
-
-    output = ""
-    output += "<li class='cards__item'>\n"
-    output += f"  <div class='card__title'>{name}</div>\n"
-    output += "  <p class='card__text'>\n"
-    output += f"    <strong>Diet:</strong> {diet}<br/>\n"
-    output += f"    <strong>Location:</strong> {location}<br/>\n"
-    output += f"    <strong>Type:</strong> {type_}<br/>\n"
-    output += "  </p>\n"
-    output += "</li>\n"
-    return output
-
-
-for animal in animals_data:
-    output = serialize_animal(animal)   
-    
-new_html = template.replace("__REPLACE_ANIMALS_INFO__", output) 
-
-with open("animals.html", "w") as file:
-    file.write(new_html) 
-    
-print("HTML file generated successfully with animal information.") 
-
+    if animals_data and template:
+        output = ""
+        for animal in animals_data:
+            output += serialize_animal(animal)
+        
+        new_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
+        
+        with open("animals.html", "w") as file:
+            file.write(new_html)
+        
+        print("HTML file generated successfully with animal information.")
+    else:
+        print("Failed to generate HTML file due to missing data or template.")
